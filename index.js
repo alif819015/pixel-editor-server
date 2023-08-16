90be0f2a69eb32044aca881977bd61faf9b25027
shahid1105 <shahidhasan1105@gmail.com>
2023-08-16 12:25:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middle ware 
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Pixel editor starts editing')
+})
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Pixel editor starts editing on port ${port}`);
+})
