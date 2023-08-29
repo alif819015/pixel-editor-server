678cc0089e88c9eea11b94e59de96f3a91da2127
ariful-islam <arifulislam204909@gmail.com>
2023-08-29 23:37:40 +0600

    git test

--- index.js
+++ index.js
@@ -25,6 +25,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+
+        const usersCollection = client.db("languageDB").collection("users");
+
+
+
+
+
+ //users related api's...........
+// .............
+    app.post("/users", async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email };
+        const existingUser = await usersCollection.findOne(query);
+  
+        if (existingUser) {
+          return res.send({ message: "user already exists" });
+        }
+  
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
+
+
+
+
+
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
